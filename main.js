4612b162f837ebcbfcf2700f3811b9b6267f0c7b
Azra Ali <azra_ali09@outlook.com>
2025-08-05 21:01:55 -0400

    added the final image for music gallery (adjusted /debugged everything)

--- main.js
+++ main.js
@@ -6,18 +6,19 @@ const flip = document.querySelector('#flip');
 const paper1 = document.querySelector('#p1');
 const paper2 = document.querySelector('#p2');
 const paper3 = document.querySelector('#p3');
+const paper4 = document.querySelector('#p4');
 
 prevBtn.addEventListener("click", goPrevious);
 nextBtn.addEventListener("click", goNext);
 
 let currentLocation = 1;
-let numOfPages = 3;
+let numOfPages = 4;
 let maxLocation = numOfPages + 1;
 //coding things for buttons (to save thing for when i fix them)
 function openFlip(){
     flip.style.transform = "translateX(50%)";
-    prevBtn.style.transform = "translateX(-180px)";
-    nextBtn.style.transform = "translateX(180px)";
+    prevBtn.style.transform = "translateX(" + (-180 - 12.5) + "px)"; 
+    nextBtn.style.transform = "translateX(" + (180 + 22.5) + "px)"; 
 
 
 }
@@ -28,8 +29,8 @@ function closeFlip(isFirstPage){
     } else{
         flip.style.transform = "translateX(100%)"
     }
-    prevBtn.style.transform = "translateX(0px)";
-    nextBtn.style.transform = "translateX(0px)";
+    prevBtn.style.transform = "translateX(-12.5px)"; 
+    nextBtn.style.transform = "translateX(12.5px)";
 }
 
 
@@ -46,9 +47,13 @@ function goNext(){
                 paper2.style.zIndex= 2;
                 break;
             case 3:
-                closeFlip(false);
                 paper3.classList.add("flipped");
                 paper3.style.zIndex= 3;
+                break;
+            case 4:
+                closeFlip(false);
+                paper4.classList.add("flipped");
+                paper4.style.zIndex= 4;
                 break;
             default:
                 throw new Error("unkown state");
@@ -65,16 +70,21 @@ function goPrevious(){
             case 2:
                 closeFlip(true);
                 paper1.classList.remove("flipped") 
-                paper1.style.zIndex = 3;
+                paper1.style.zIndex = 4;
                 break;
             case 3: 
                 paper2.classList.remove("flipped");
-                paper2.style.zIndex= 2 ;
+                paper2.style.zIndex= 3 ;
                 break;
-            case 4:
-                openFlip();
+            case 4: 
                 paper3.classList.remove("flipped");
-                paper3.style.zIndex = 1;
+                paper3.style.zIndex= 2 ;
+                break;
+
+            case 5:
+                openFlip();
+                paper4.classList.remove("flipped");
+                paper4.style.zIndex = 1;
                 break;
             default:
                 throw new Error("unkown state");
